09f22f753f15318c9a09979c4c06c2cf8a225940
Joclrk <jocyber2021@gmail.com>
2026-09-13 18:10:11 -0400

    Section 2.2 Update

--- Assets/Script/Driver.cs
+++ Assets/Script/Driver.cs
@@ -1,16 +1,16 @@
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Driver : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(0, 0, 100);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 90);
     }
 }
