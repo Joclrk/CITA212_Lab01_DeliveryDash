e4a709abccd2662eab9ca552e2bb8772e0f10ece
Joclrk <jocyber2021@gmail.com>
2026-09-13 21:39:17 -0400

    Section 2.6 and 2.7 Update
    Added a moving car with  tank control movement.

--- Assets/Script/Driver.cs
+++ Assets/Script/Driver.cs
@@ -1,20 +1,39 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Driver : MonoBehaviour
 {
     [SerializeField] float steer = 0.5f;
     [SerializeField] float speed = 0.05f;
-   
-
-    void Start()
-    {
-        //transform.Rotate(0, 0, 100);
-    }
-
-    // Update is called once per frame
+    //void Start() useless for now but maybe used later.
+    //{
+    //}
     void Update()
     {
-        transform.Rotate(0, 0,steer);
-        transform.Translate(0,speed,0);
+        //Everchanging variables C = Changing variable
+        //right/left
+        float Csteer = 0f;
+        //up//down
+        float Cspeed = 0f;
+        //Movement Keys
+        if (Keyboard.current.wKey.isPressed)
+        {
+            Cspeed = 1f;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            Csteer = -1f;
+        }
+        if (Keyboard.current.aKey.isPressed)
+        {
+            Csteer = 1f;
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            Cspeed = 0f;
+        }
+
+        transform.Rotate(0, 0,steer * Csteer);
+        transform.Translate(0,speed * Cspeed,0);
     }
 }
