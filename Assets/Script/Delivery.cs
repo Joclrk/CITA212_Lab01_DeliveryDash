505e6b87753aa510001585aca6df0e0a813c580d
Joclrk <jocyber2021@gmail.com>
2026-09-22 21:57:44 -0400

    Section 2.19, 2.20, 2.21 Update
    Added Prefabs, Made package destroy  on pickup and trigger sparkles when picked up.

--- Assets/Script/Delivery.cs
+++ Assets/Script/Delivery.cs
@@ -3,22 +3,25 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool HasPackage;
-    //float health = 3f;
+    
     void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("collision");
     }
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision) 
     {
-        if (collision.CompareTag("Package"))
+        if (collision.CompareTag("Package") && !HasPackage)
         {
             Debug.Log("package grabbed");
             HasPackage = true;
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject);
         }
         if (collision.CompareTag("Custom") && HasPackage)
         {
             Debug.Log("Package Delivered");
             HasPackage = false;
+            GetComponent<ParticleSystem>().Stop();
         }
     }
 }
